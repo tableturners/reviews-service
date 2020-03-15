e0230b4ce24073e003765c357d1495bb2d4baafc
Alex Cai <yhcai@ucdavis.edu>
2020-03-15 16:09:45 -0700

    set up css loaders.

--- opentable_rv/client/src/ReviewEntry.jsx
+++ opentable_rv/client/src/ReviewEntry.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
+import "./ReviewEntry.css";
 
 const ReviewEntry = (props) => {
  // console.log('ggggg', props)
   return (
     <div className="txn-row">
-      <div className="txn-data">{props.list.username}</div>
+        
+      <div className="txn-data username">{props.list.username}</div>
       <div className="txn-data">{props.list.place}</div>
       <div className="txn-data">{props.list.DinedDate}</div>
       <div className="txn-data">{props.list.postedReview}</div>
+      <div className="txn-data">{props.list.paragraph}</div>
+      <div className="txn-data">{props.list.overall}</div>
+      <div className="txn-data">{props.list.food}</div>
+      <div className="txn-data">{props.list.ambience}</div>
+      <div className="txn-data">{props.list.service}</div>
+      <div className="txn-data">{props.list.star}</div>
     </div>
   )
 
--- opentable_rv/webpack.config.js
+++ opentable_rv/webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
   },
   module: {
     rules: [
+      { test: /\.css$/, loader: "style-loader!css-loader" },
       {
         test: /.js$|.jsx$/,
         exclude: /node_modules/,
